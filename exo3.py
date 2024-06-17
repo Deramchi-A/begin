d0aac8749fa9ceb09ac4864f9d9e092255d3f533
Deramchi-A <hamidnba06@gmail.com>
2024-06-17 11:23:20 +0100

    add an exo 4

---
+++ exo3.py
@@ -0,0 +1,13 @@
+def even_char(num_char):#definetion of the func
+    num_list=[]#an empty list
+    n=0# the steps 
+    for i in num_char:# the for loop to create 
+                       #a list with seperate characters
+        num_list.append(i)
+    for i in num_list[n::n+2]:# print the even elementes in the list
+        print(i)
+    
+    print(num_list)
+
+
+even_char("hamidderamchi")
---
+++ exo4.py
@@ -0,0 +1,3 @@
+def remove_chars(chr_list,n=int):
+    for i in chr_list:
+        print(chr_list())
